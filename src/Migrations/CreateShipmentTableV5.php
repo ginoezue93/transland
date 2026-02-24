e441eeabeb480de09e3e9c6f41afad374f8ef77b
Görkem Inözü <goerkem1@uni-bremen.de>
2026-02-24 19:53:53 +0100

    lets go

--- src/Migrations/CreateShipmentTableV5.php
+++ src/Migrations/CreateShipmentTableV5.php
@@ -5,14 +5,11 @@ namespace TranslandShipping\Migrations;
 use TranslandShipping\Models\Shipment;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
 
-class CreateShipmentTableV4
+class CreateShipmentTableV5
 {
     public function run(Migrate $migrate): void
     {
-        // Alte Tabelle löschen (alle alten Daten sind ungültig)
         $migrate->deleteTable(Shipment::class);
-
-        // Neu anlegen mit korrekten Spalten
         $migrate->createTable(Shipment::class);
     }
 }
